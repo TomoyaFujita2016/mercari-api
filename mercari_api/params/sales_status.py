9a02a69935ee14ea31a341e6261e84ac81997ff4
TomoyaFujita2016 <fujita.t.2016c@gmail.com>
2021-10-04 20:37:03 +0900

    del all(ui refactoring)

--- mercari_api/params/sales_status.py
+++
@@ -1,7 +0,0 @@
-from .simple_enum import SimpleEnum
-
-
-class SalesStatus(SimpleEnum):
-    ON_SALE = ("status_on_sale", "販売中")
-    SOLD_OUT = ("status_trading_sold_out", "売り切れ")
-    ALL = ("status_all", "すべて")
--- mercari_api/params/shipping_payer.py
+++
@@ -1,7 +0,0 @@
-from .simple_enum import SimpleEnum
-
-
-class ShippingPayer(SimpleEnum):
-    ALL = ("shipping_payer_all", "すべて")
-    CASH_ON_SHIPPING = ("shipping_payer_id[1]", "着払い(購入者負担)")
-    FEE_INCLUDED = ("shipping_payer_id[2]", "送料込み(出品者負担)")
